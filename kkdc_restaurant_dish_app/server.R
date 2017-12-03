8cea8ed88935aa60ac23f0efe2f695ff74ac865f
kevinwork98 <kevinwork98@gmail.com>
2017-12-03 13:20:09 -0800

    Filtered datasets to New York, includes code for filtering

--- kkdc_restaurant_dish_app/server.R
+++ kkdc_restaurant_dish_app/server.R
@@ -14,14 +14,32 @@ library(dplyr)
 # Change these to reflect the path to the files on your local machine
 # May or may not need to find a way to upload compressed files and unzip
 # For use in the shiny app
-dishes <- read.csv("data/Dish.csv")
-menues <- read.csv("data/Menu.csv")
+'dishes <- read.csv("data/Dish.csv")
+menus <- read.csv("data/Menu.csv")
 menu_items <- read.csv("data/MenuItem.csv")
-menu_pages <- read.csv("data/MenuPage.csv")
+menu_pages <- read.csv("data/MenuPage.csv")'
+
+# filter menu datasets
+# possibility of making this reactively filter to user-specified location?
+'menu_filter <- filter(menus, grepl("NY", place))
+page_filter <- menu_pages[menu_pages$menu_id %in% menu_filter$id, ]
+item_filter <- menu_items[menu_items$menu_page_id %in% page_filter$id, ]
+dish_filter <- dishes[dishes$id %in% item_filter$dish_id, ]'
+
+# save filtered datasets to new sets
+'write.csv(menu_filter, "data/new_york_menus.csv")
+write.csv(page_filter, "data/new_york_pages.csv")
+write.csv(item_filter, "data/new_york_items.csv")
+write.csv(dish_filter, "data/new_york_dishes.csv")'
+#commented out
 
 # Define non-reactive functions here
+#
+#
+#
 
-# Define server logic to reactively respond to user desires
+
+# Define server functions that reactively respond to user queries
 shinyServer(function(input, output) {
   output$value <- renderPrint({
     input$text
